e8773956a71288e93ced27b1e56b1d650ad0db9c
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2021-06-06 00:19:45 +0530

    create regular interval array

---
+++ NpArray3.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#create arrray of regular interval
+
+np.arange(10)
+
+np.arange(2,3,0.1) # (start,end,interval) here end is not included
+
+np.linspace(1,4,6) #(start,end,No. of elem.). It will automatically divide into given parts of equal length.
