8ac9f6486248471de927136ac2b90d199f0fb3fc
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2021-06-06 00:05:13 +0530

    Slice string function

---
+++ SliceString.py
@@ -0,0 +1,12 @@
+s = "Welcome to My World" # s= .....0, 1,2,3,4,5,....
+print(s)
+
+print(s[2:5]) # slicing of string, colon is slicing operator
+
+s[:5] # it doesn't include outer bond
+
+s[1:8:2] #s[start : end: increment/decrement]
+
+s[::-1] #reverse of the string
+
+s[-1:-4:-1] # s[start: end: increment]
