bc0c124a54a1c75743942911c8a20fd11c5e7d52
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 23:03:00 +0530

    create automatic array with range

--- Numpy/AutoArray.py
+++ Numpy/AutoArray.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-#It will automatically divide into given parts of equal length.
+# It will automatically divide into given parts of equal length.
 
 np.linspace(1,4,6) #(start,end,No. of elem.).
