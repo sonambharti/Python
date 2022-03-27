b971abaa615598d4ae7856531e85c1d0912de9ba
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-28 00:13:47 +0530

    Delete LinearPlot3.py

--- LinearPlot3.py
+++
@@ -1,8 +0,0 @@
-#set line width of linear plot
-
-from matplotlib import pyplot as plt
-
-x2=[6,9,11]
-y2=[6,15,7]
-
-plt.plot(x2,y2,'g',label='line',linewidth=5) #(x-coordinate,y-coordinate,'color code for line',line-width)
