3b15380c889d2620e26b2ee1c84760173b6e0359
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 23:19:38 +0530

    Delete HistogramEx2.py

--- HistogramEx2.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import  pyplot as plt
-
-age = [2,5,3,5,5,4,4,2,2,5,5,4,18,12,16,21,19,24,18,17,17,21,24,27,19,17,15,19,24,22,23,19,25,26,27,30]
-bins = [0,5,10,15,20,25,30,35]
-
-plt.hist(age,bins,histtype='step') #(data,range of data, histogram type)
-
-plt.show()
