d78514d56a2299dea49f8d254ba4a7f45ad962b4
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 23:00:04 +0530

    Delete ArrPlot.py

--- ArrPlot.py
+++
@@ -1,8 +0,0 @@
-#creating a numpy array and plot a graph.
-
-import numpy as np
-from matplotlib import pyplot as plt
-
-t = np.arange(0,5,0.2) #creating a numpy array from range 0-5 with difference of 0.2
-plt.plot(t,t,'r--') #here '--' is line-connector btwn two points of a graph
-plt.show()
