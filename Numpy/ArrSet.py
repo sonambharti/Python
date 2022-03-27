3b2ef4af58e949522c9c798115275df53ead9d14
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 22:55:30 +0530

    Add files via upload

---
+++ Numpy/ArrSet.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#creates a set of arrays
+
+#It will give variations of possible arrays
+print(np.indices((3,3))) # 3 rows and 3 col
+
+print(np.indices((2,3)))
+
+print()
+
+a = np.array([1,2,3])
+print(a)
+
+print(a.dtype) #prints data type of array along with the size
---
+++ Numpy/NpOperation.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([[1,2,3],[10,20,30]])
+
+print("array dimension: ",a.ndim) #prints dimension of the particular array
+
+print("array item size: ",a.itemsize) #bit of each element
+
+print("array element data type: ",a.dtype)
+
+print("Array shape: ",a.shape)
+
+print("No. of elem in array: ",a.size)
