d19cd7300ad21f2eb63985d49bb344d0db0a1857
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 22:51:36 +0530

    Add files via upload

---
+++ Basic Projects/DefangIP.py
@@ -0,0 +1,17 @@
+# A user’s IP address is defanged to prevent the
+# user from clicking on a malicious link.
+
+def defangIP(address):
+    new_address = ""
+    split_address = address.split('.')
+    print(split_address)
+    print(type(split_address))
+    seprator = "[.]"
+    new_address = seprator.join(split_address)
+    return new_address
+
+if __name__ == "__main__":
+    address = input("Enter your ip address:\n")
+    ipaddress = defangIP(address)
+    print("new IP address: ", ipaddress)
+
