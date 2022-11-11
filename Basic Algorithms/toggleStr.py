e5845acf561e96c26b97ee6ea50c4403c467926a
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-11-11 23:47:57 +0530

    Convert characters of a string to opposite case

--- Basic Algorithms/toggleStr.py
+++ Basic Algorithms/toggleStr.py
@@ -10,3 +10,19 @@ for i in range(len(str)):
         res += (str[i].lower())
         
 print(res)
+
+"""
+#2nd method
+
+res = list(str)
+for i in range(len(str)):
+    if res[i]>='a' and res[i]<='z':
+        res[i] = chr(ord(res[i])-32)
+        
+    elif res[i]>='A' and res[i]<='Z':
+        res[i] = chr(ord(res[i])+32)
+        
+str = ''.join(res)
+print(str)
+
+"""
