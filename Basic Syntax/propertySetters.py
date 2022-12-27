0f37ef52f5ab27df1c701b2b9f0d67061974c910
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-12-28 01:27:29 +0530

    Example of setters and deleter

---
+++ Basic Syntax/propertySetters.py
@@ -0,0 +1,45 @@
+#Example of setters and deleter
+
+class Employee:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+        # self.email = f"{fname}.{lname}@codewithharry.com"
+
+    def explain(self):
+        return f"This employee is {self.fname} {self.lname}"
+
+    @property
+    def email(self):
+        if self.fname==None or self.lname == None:
+            return "Email is not set. Please set it using setter"
+        return f"{self.fname}.{self.lname}@codewithharry.com"
+
+    @email.setter
+    def email(self, string):
+        print("Setting now...")
+        names = string.split("@")[0]
+        self.fname = names.split(".")[0]
+        self.lname = names.split(".")[1]
+
+    @email.deleter
+    def email(self):
+        self.fname = None
+        self.lname = None
+
+
+sonam_bharti = Employee("Sonam", "Bharti")
+
+
+print(hindustani_supporter.email)
+
+hindustani_supporter.fname = "US"
+
+print(hindustani_supporter.email)
+hindustani_supporter.email = "this.that@codewithharry.com"
+print(hindustani_supporter.fname)
+
+del hindustani_supporter.email
+print(hindustani_supporter.email)
+hindustani_supporter.email = "Harry.Perry@codewithharry.com"
+print(hindustani_supporter.email)
