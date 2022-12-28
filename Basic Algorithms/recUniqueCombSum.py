918ec9ad3658874eeef06e625513c860b994a7f4
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-12-28 16:25:32 +0530

    Print unique subseq of a list to get a target sum

--- Basic Algorithms/recUniqueCombSum.py
+++ Basic Algorithms/recUniqueCombSum.py
@@ -31,3 +31,32 @@ if __name__ == "__main__":
     combSum(0, 7, lst, arr, n)
     print(res)
     
+
+"""
+res = []
+class Solution:
+    def combSum(self, indx, target, lst, arr, n):
+        global res
+        if indx == n:
+            if target == 0:
+                #print(arr)
+                if list(arr) not in res:
+                    res.append(list(arr))
+            return
+            
+        if lst[indx] <= target:
+            arr.append(lst[indx])
+            self.combSum(indx+1, target-lst[indx], lst, arr, n)
+            arr.remove(arr[len(arr)-1])
+            
+        self.combSum(indx+1, target, lst, arr, n)
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        arr = []
+        global res
+        res = []
+        candidates.sort()
+        n = len(candidates)
+        self.combSum(0, target, candidates, arr, n)
+        return res
+    """
