05df5207472d70cb541b01296c6b1fcf586564d5
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2023-01-02 23:47:55 +0530

    Count the number of subarrays with given XOR K

--- Basic Algorithms/subarrTargetXOR.py
+++ Basic Algorithms/subarrTargetXOR.py
@@ -6,27 +6,17 @@ A subarray is a contiguous part of array.
 
 def subarrayTargetXOR(A, B):
     n = len(A)
-    temp = []
-    lst = []
     count = 0
     n = len(A)
     for i in range(n):
-        for j in range(i,n+1):
-            for k in range(i,j):
-                temp.append(A[k])
-            if temp:
-                lst.append(temp)
-            temp = []
-                
-    for i in range(len(lst)):
-        arr = lst[i]
         s = 0
-        for j in range(len(arr)):
-            s = s^arr[j]
-        if s == B:
-            count += 1
+        for j in range(i,n):
+            s = s^A[j]
+            if s == B:
+                count += 1
        
     return count
+    
     
     
 A = []
