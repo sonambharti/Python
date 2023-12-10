c540a11039bb164d94491bc65586073c8090516e
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2023-12-10 21:06:03 +0530

    BFS Implementations using Doubly-end-Queue (Deque).

---
+++ Graph/BFS.py
@@ -0,0 +1,66 @@
+# BFS Implementations using Doubly-end-Queue (Deque).
+
+from collections import deque
+
+def create_adj(edges):
+    print("Creating directed adjacency Matrix")
+    numOfNodes = 1 + max([el[1] for el in edges] + [el[0] for el in edges])
+    adj = [[0] * numOfNodes for _ in range(numOfNodes)]
+    
+    for e in edges:
+        adj[e[0]][e[1]] = 1 
+    
+    return adj
+    
+    
+def create_adj_undirected(edges):
+    print("Creating directed adjacency Matrix")
+    numOfNodes = 1 + max([el[1] for el in edges] + [el[0] for el in edges])
+    adj = [[0] * numOfNodes for _ in range(numOfNodes)]
+    
+    for e in edges:
+        adj[e[0]][e[1]] = adj[e[1]][e[0]] = 1 
+    
+    return adj
+  
+
+def bfs(adjMat):
+    # using BFS
+    print("BFS Traversal: \n")
+    vis = set()
+    adj = adjMat
+    n = len(adj)
+    
+    for i in range(n):
+        if i in vis:
+            continue
+        
+        vis.add(i)
+        q = deque([i])
+        while q:
+            el = q.pop()
+            print(el, end=" ")
+            for adjel in range(n):
+                if adj[el][adjel]==1 and adjel not in vis:
+                    vis.add(adjel)
+                    q.appendleft(adjel)
+    print()
+
+
+if __name__ == "__main__":
+  adjMat_dir = create_adj([[0,1], [1,2], [1,3], [1,4], [3,4], [5,7], [7,6]])
+  print("Directed Adjecency Matrix....\n")
+  for x in adjMat_dir:
+      print(x)
+  
+  print("\nPrinting Directed BFS Traversal...\n")
+  bfs(adjMat_dir)
+  
+  
+  adjMat_undir = create_adj_undirected([[0,1], [1,2], [1,3], [1,4], [3,4], [5,7], [7,6]])
+  print("\n\nUn-directed Adjecency Matrix....\n")
+  for x in adjMat_undir:
+      print(x)
+  
+  print("\nPrinting Un-directed BFS Traversal...\n")
+  bfs(adjMat_undir)
