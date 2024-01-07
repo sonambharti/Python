24d8c10460f08cd7a90ee4bca0ddea9012e6f366
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-01-07 23:02:47 +0530

    Minimize Max Array

---
+++ Basic Algorithms/minimizeMaxArr.py
@@ -0,0 +1,33 @@
+"""
+You are given a 0-indexed array nums comprising of n non-negative integers.
+
+In one operation, you must:
+
+Choose an integer i such that 1 <= i < n and nums[i] > 0.
+Decrease nums[i] by 1.
+Increase nums[i - 1] by 1.
+Return the minimum possible value of the maximum integer of nums after performing any number of operations.
+
+ 
+
+Example 1:
+
+Input: nums = [3,7,1,6]
+Output: 5
+"""
+from math import ceil
+
+def minimizeArrayValue(nums):
+    sum = 0
+    ans = 0
+    n = len(nums)
+    for i in range(n):
+        sum += nums[i]
+        avg = sum/(i+1)
+        if avg > ans:
+            ans = avg
+    return ceil(ans)
+    
+nums = [3,7,1,6]
+res = minimizeArrayValue(nums)
+print(res)
