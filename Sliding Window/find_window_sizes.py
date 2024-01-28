7f06b928f15ef7b5a9fc6e53d1546b580d317884
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-01-28 19:39:55 +0530

    Find the window sizes which contains the required  sum

--- Sliding Window/find_window_sizes.py
+++ Sliding Window/find_window_sizes.py
@@ -20,7 +20,8 @@ def find_window_sizes_bruteforce(arr, target_sum):
             if current_sum == target_sum:
                 window_indx.append((i, j))
                 window_sizes.append(j-i+1)
-
+                
+    print("In Brute force", window_indx)
     return window_sizes
 
 
@@ -40,12 +41,13 @@ def find_window_sizes_sliding_window(arr, target_sum):
         if current_sum == target_sum:
             window_indx.append((left, right))
             window_sizes.append(right-left+1)
-
+            
+    print("In Sliding Window",window_indx)
     return window_sizes
     
 
 if __name__ == "__main__":
-    arr = [1, 4, 2, 10, 2, 3, 1, 0, 20]
+    arr = [6, 4, 2, 10, 2, 3, 1, 0, 20]
     target_sum = 12
     result_bruteforce = find_window_sizes_bruteforce(arr, target_sum)
     print("Window Sizes (Brute Force):", result_bruteforce)
