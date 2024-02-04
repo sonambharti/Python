6d2f7a88ae49042148d4ab813857d3549db2b34b
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-02-04 13:33:39 +0530

    count of the occurrences of anagrams of the word in the text

--- Sliding Window/search_anagram_count.py
+++ Sliding Window/search_anagram_count.py
@@ -29,14 +29,14 @@ def search_anagram_count(pat, txt):
     for word in pat:
         word_count[word] = 1 + word_count.get(word, 0)
     i, j, ans = 0, 0, 0
-    n = len(pat)
+    k = len(pat)
     count = len(word_count)
     while j < len(txt):
         if txt[j] in word_count:
             word_count[txt[j]] -= 1
             if word_count[txt[j]] == 0:
                 count -= 1
-        if j - i + 1 == n:
+        if j - i + 1 == k:
             if count == 0:
                 ans += 1
             if txt[i] in word_count:
