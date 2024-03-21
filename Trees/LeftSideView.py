90ead3d2e799a31cb8e2050ad1e92c14fd3eee1d
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-03-21 23:44:49 +0530

    return a list containing elements of left view of the binary tree

---
+++ Trees/LeftSideView.py
@@ -0,0 +1,83 @@
+"""
+/*
+Given the root of a binary tree, imagine yourself standing on the left side of it.
+Return the values of the nodes you can see ordered from top to bottom.
+
+
+    5			<-- 5		
+   / \
+  1   8			<-- 1
+   \   \
+    4   9		<-- 4
+       /
+      2			<-- 2
+"""
+# Function to return a list containing elements of left view of the binary tree.
+from collections import deque
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+def LeftView(root):
+    if not root:
+        return []
+    level = []
+    res = []
+    Q = deque([None, root])
+    
+    while Q:
+        x = Q.pop()
+        if x:
+            level.append(x.data)
+            if x.left:
+                Q.appendleft(x.left)
+            if x.right:
+                Q.appendleft(x.right)
+                
+        else:
+            #lst.append(level)
+            res.append(level[0])
+            level = []
+            
+            if Q:
+                Q.appendleft(None)
+    return res
+    
+def optimized_LeftView(root):
+    # code here
+    if not root:
+        return []
+    level = []
+    res = []
+    Q = deque([root])
+    
+    while Q:
+        level_size = len(Q)
+        
+        for i in range(level_size):
+            node = Q.popleft()
+            if i == 0:  # Only consider the first node at each level
+                res.append(node.data)
+            
+            if node.left:
+                Q.append(node.left)
+            if node.right:
+                Q.append(node.right)
+    
+    return res
+    
+root = Node(5)
+root.left = Node(1)
+root.right = Node(8)
+root.left.right = Node(4)
+root.right.right = Node(9)
+root.right.right.left = Node(2)
+
+res = LeftView(root)
+print(res)
+
+res1 = optimized_LeftView(root)
+print(res)
