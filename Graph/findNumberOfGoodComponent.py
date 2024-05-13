0013ac3750f7fa8dd0c79e45ba2372bb9fe92e2b
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-05-14 00:39:44 +0530

    find no of good components in a graph using DFS graph traversal

--- Graph/findNumberOfGoodComponent.py
+++ Graph/findNumberOfGoodComponent.py
@@ -26,41 +26,52 @@ Explanation:
 We can see that there are 3 components in the graph. For 1-2-7 there is no edge between 1 to 7, 
 so it is not a fully connected component. Rest 2 are individually fully connected component.
 """
+
 from typing import List
 from collections import defaultdict
 
-
 class Solution:
+    # Function to find the number of good components in a given undirected graph
     def findNumberOfGoodComponent(self, e : int, v : int, edges : List[List[int]]) -> int:
-        # code here
+        # Create an adjacency list to represent the graph
         graph = defaultdict(list)
         for fro, to in edges:
             graph[fro].append(to)
             graph[to].append(fro)
-            
+
+        # Define a helper function for Depth-First Search (DFS)
         def dfs(vertex, nei):
-            nonlocal flag, count
+            # Mark the current vertex as visited
             vis.add(vertex)
+            # Increment the count of vertices in the current component
             count += 1
-            if len(graph[vertex]) != nei:
+            # Check if the current component is fully connected
+            if len(graph[vertex])!= nei:
                 flag = False
+            # Recursively visit the adjacent vertices
             for n in graph[vertex]:
                 if n not in vis:
                     dfs(n, nei)
+            # Update the flag based on the connectivity of the current component
             flag = flag & True
+
+        # Initialize the answer as 0
         ans = 0
+        # Initialize a set to keep track of visited vertices
         vis = set()
+        # Iterate through all the vertices
         for i in range(1, v+1):
-            # print(vis)
-            nei = len(graph[i])
+            # Initialize the count of vertices in the current component
             count = -1
+            # Initialize the flag to check if the current component is fully connected
             flag = True
+            # If the vertex is not visited, perform DFS
             if i not in vis:
-                dfs(i, nei)
-                # print(count)
-                if flag and count == nei:
-                    # print(i)
+                dfs(i, len(graph[i]))
+                # If the current component is fully connected, increment the answer
+                if flag and count == len(graph[i]):
                     ans += 1
+        # Return the answer
         return ans
 
 
