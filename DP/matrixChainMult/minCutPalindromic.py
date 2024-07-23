1c524e5725909a6b942ac7dc0faade5219be407d
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-07-23 22:11:54 +0530

    Return the minimum cuts needed for a palindrome partitioning of s

--- DP/matrixChainMult/minCutPalindromic.py
+++ DP/matrixChainMult/minCutPalindromic.py
@@ -16,9 +16,10 @@ Output: 0
 """
 MOD = 10**9+7
 
-def minCutMatrixMult(s):
-    n = len(s)
-    dp = [-1]*(n+1)
+def minCutMatrixMult(string):
+    # code here
+    n = len(string)
+    dp = [[-1]*(n) for _ in range(n)]
     def is_palindrome(string, i, j):
         if i>= j:
             return True
@@ -31,34 +32,34 @@ def minCutMatrixMult(s):
         
     def rec_palindromicPartition(string, i, j):
         global MOD
-            if i>=j:
-                dp[i][j] = 0
-            if is_palindrome(string, i, j) == True:
-                dp[i][j] = 0
-            if dp[i][j] != -1:
-                return dp[i][j]
-                
-            dp[i][j] = MOD
-            for k in range(i, j):
-                # minm_part = min(minm_part, 1 + rec_palindromicPartition(string, i, k) + rec_palindromicPartition(string, k+1, j))
-                if dp[i][k] != -1:
-                    left = dp[i][k]
-                else:
-                    left = rec_palindromicPartition(string, i, k)
-                    dp[i][k] = left
-                if dp[k+1][j] != -1:
-                    right = dp[k+1][j]
-                else:
-                    right = rec_palindromicPartition(string, k+1, j)
-                    dp[k+1][j] = right
-                dp[i][j] = min(dp[i][j], 1 + left + right)
+        if i>=j:
+            dp[i][j] = 0
+        if is_palindrome(string, i, j) == True:
+            dp[i][j] = 0
+        if dp[i][j] != -1:
             return dp[i][j]
-        return rec_palindromicPartition(string, 0, n-1)
-
+            
+        dp[i][j] = MOD
+        for k in range(i, j):
+            # minm_part = min(minm_part, 1 + rec_palindromicPartition(string, i, k) + rec_palindromicPartition(string, k+1, j))
+            if dp[i][k] != -1:
+                left = dp[i][k]
+            else:
+                left = rec_palindromicPartition(string, i, k)
+                dp[i][k] = left
+            if dp[k+1][j] != -1:
+                right = dp[k+1][j]
+            else:
+                right = rec_palindromicPartition(string, k+1, j)
+                dp[k+1][j] = right
+            dp[i][j] = min(dp[i][j], 1 + left + right)
+        return dp[i][j]
+    return rec_palindromicPartition(string, 0, n-1)
+        
 def minCutPalindromic(s):
 
     n = len(s)
-    dp = [[-1] * n for _ in range(n)]
+    dp = [-1] * n
     P = [[False] * n for _ in range(n)]
     
     # Every substring of length 1 is a palindrome
@@ -77,17 +78,24 @@ def minCutPalindromic(s):
     # Fill the dp table for the minimum cuts
     for i in range(n):
         if P[0][i]:
-            dp[0][i] = 0
+            dp[i] = 0
         else:
-            dp[0][i] = MOD
+            dp[i] = MOD
             for j in range(i):
-                if P[j + 1][i] and dp[0][j] + 1 < dp[0][i]:
-                    dp[0][i] = dp[0][j] + 1
+                if P[j + 1][i] and dp[j] + 1 < dp[i]:
+                    dp[i] = dp[j] + 1
+                    
+    print("Palindrome Table:\n")
+    for i in range(n):
+        print(P[i])
+    print("\n-----------------------------------\n")
+    print("DP Table:\n")
+    print(dp)
     
-    return dp[0][n - 1]
+    return dp[n - 1]
     
 if __name__ == "__main__":
-    s = "aab"
+    s = "aabackca"
     res1 = minCutMatrixMult(s)
     print("All possible palindrome partitioning of s using Matrix Multiplication: ", res1)
     res2 = minCutPalindromic(s)
