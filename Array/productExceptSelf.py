0f3ad82a22643e3f051d19e330250eda623acae2
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-08-16 13:35:32 +0530

    Product of array except itself

---
+++ Array/productExceptSelf.py
@@ -0,0 +1,39 @@
+"""
+# Product of array except itself
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the 
+product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+"""
+def productExceptSelf(nums):
+    n = len(nums) 
+    res = [0]*n
+    prefix_prod, postfix_prod = 1, 1
+    for i in range(n):
+        res[i] = prefix_prod
+        prefix_prod *= nums[i]
+
+    for i in range(n-1, -1, -1):
+        res[i] *= postfix_prod
+        postfix_prod *= nums[i]
+    return res
+        
+        
+if __name__ == "__main__":  
+    nums = [1, 5, 3, 0, 2]
+    res = productExceptSelf(nums)
+    print("Maximum product = ", res)
