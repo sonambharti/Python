9b9d31550d9e81b7c9427d1c7c2bad360f087db3
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-08-23 21:27:04 +0530

    Search in a Binary Search Tree

---
+++ Trees/searchBST.py
@@ -0,0 +1,87 @@
+"""
+# 700. Search in a Binary Search Tree
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree 
+rooted with that node. If such a node does not exist, return null.
+
+Examples:
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+                   5		
+                 /   \      
+                2    12		
+               / \   / \    
+              1   3 9  21	                    
+                      / \               
+                     19	25	 
+
+"""
+from collections import deque
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+def levelOrder(root):
+    level = []
+    lst = []
+    
+    if not root:
+        return root
+        
+    Q = deque([None, root])
+    
+    while (True):
+        x = Q.pop()
+        if x:
+            level.append(x.data)
+            if x.left:
+                Q.appendleft(x.left)
+            if x.right:
+                Q.appendleft(x.right)
+        
+        else:
+            lst.append(level)
+            level = []
+            
+            if Q:
+                Q.appendleft(None)
+            else:
+                break
+            
+    return lst
+    
+    
+def searchBST(root, val):
+    if not root:
+        return None
+    
+    if root.data == val:
+        return levelOrder(root)
+        
+    elif root.data > val:
+        return searchBST(root.left, val)
+    else:
+        return searchBST(root.right, val)    
+        
+    
+if __name__ == "__main__":
+    root = Node(5)
+    root.left = Node(2)
+    root.right = Node(12)
+    root.left.left = Node(1)
+    root.left.right = Node(3)
+    root.right.left = Node(9)
+    root.right.right = Node(21)
+    root.right.right.left = Node(19)
+    root.right.right.right = Node(25)
+    
+    
+    
+    res1 = searchBST(root, 21)
+    print("Maxm Sum Path: ", res1)
