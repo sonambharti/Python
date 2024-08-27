a6dddaf1e4effb8a4941ef285b87030bbce24889
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-08-27 22:55:02 +0530

    count the number of nodes in given Binary Tree

---
+++ Trees/CountTreeNode.py
@@ -0,0 +1,111 @@
+"""
+# Size of Binary Tree
+
+Given a binary tree of size n, you have to count the number of nodes in it. For example, the count of nodes in the tree below is 4.
+
+        1
+     /      \
+   10      39
+  /
+5
+
+Example 1:
+Input:
+1 2 3
+Output:
+3
+Explanation:
+Given Tree is :
+                              1
+                             /  \
+                            2   3
+There are 3 nodes in the tree.
+Example 2:
+Input:
+10 5 9 N 1 3 6 
+Output:
+6
+Explanation:
+Given Tree is :
+                              10
+                             /  \
+                           5     9
+                            \    / \
+                             1 3   6
+There are 6 nodes in the tree.
+
+
+
+                   5		
+                 /   \      
+                2    12		
+               / \   / \    
+              1   3 9  21	                    
+                      / \               
+                     19	25	 
+
+"""
+from collections import deque
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+def levelOrder(root):
+    level = []
+    lst = []
+    
+    if not root:
+        return root
+        
+    Q = deque([None, root])
+    
+    while (True):
+        x = Q.pop()
+        if x:
+            level.append(x.data)
+            if x.left:
+                Q.appendleft(x.left)
+            if x.right:
+                Q.appendleft(x.right)
+        
+        else:
+            lst.append(level)
+            level = []
+            
+            if Q:
+                Q.appendleft(None)
+            else:
+                break
+            
+    return lst
+  
+  
+def getSize(node):
+    # code here
+    if not node:
+        return 0
+    res = 1 + getSize(node.left) + getSize(node.right)
+        
+    return res
+        
+if __name__ == "__main__":
+    root = Node(5)
+    root.left = Node(2)
+    root.right = Node(12)
+    root.left.left = Node(1)
+    root.left.right = Node(3)
+    root.right.left = Node(9)
+    root.right.right = Node(21)
+    root.right.right.left = Node(19)
+    root.right.right.right = Node(25)
+    
+    
+    
+    res1 = getSize(root)
+    print("Size of Binary Tree: ", res1)
+    
+    
+    
