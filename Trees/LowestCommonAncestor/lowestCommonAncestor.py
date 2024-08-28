e09f6889ff3420624c0e0580744ebf2fd76485bc
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-08-28 17:34:25 +0530

    Lowest Common Ancestor of a Binary Tree

--- Trees/LowestCommonAncestor/lowestCommonAncestor.py
+++ Trees/LowestCommonAncestor/lowestCommonAncestor.py
@@ -1,32 +1,29 @@
 """
-# 235. Lowest Common Ancestor of a Binary Search Tree
+# 236. Lowest Common Ancestor of a Binary Tree
 
-Given a binary search tree (BST), find the lowest common ancestor (LCA) node of
-two given nodes in the BST.
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
-According to the definition of LCA on Wikipedia: “The lowest common ancestor 
-is defined between two nodes p and q as the lowest node in T that has both 
-p and q as descendants (where we allow a node to be a descendant of itself).”
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the
+lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
  
 
 Example 1:
 
 
-Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
-Output: 6
-Explanation: The LCA of nodes 2 and 8 is 6.
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
 Example 2:
 
 
-Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
-Output: 2
-Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant 
-of itself according to the LCA definition.
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
 Example 3:
 
-Input: root = [2,1], p = 2, q = 1
-Output: 2
+Input: root = [1,2], p = 1, q = 2
+Output: 1
 
 
 
