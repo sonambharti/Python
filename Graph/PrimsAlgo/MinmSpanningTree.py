5157684ec8b849c3f2d5871ceff8468fce7fd5f5
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-08-29 22:48:17 +0530

    Implemented Minimum Spaning tree using Prims algorithm

--- Graph/PrimsAlgo/MinmSpanningTree.py
+++ Graph/PrimsAlgo/MinmSpanningTree.py
@@ -1,11 +1,19 @@
 '''
 #  Minimum Spanning Tree 
 
+Spanning Tree - A spanning tree is a subset of Graph G, such that all the vertices are connected
+using minimum possible number of edges. Hence, a spanning tree does not have cycles and a graph 
+may have more than one spanning tree.
+
+Note: A graph can have multiple spanning tree.
+
 Given a weighted, undirected, and connected graph with V vertices and E edges, your task 
 is to find the sum of the weights of the edges in the Minimum Spanning Tree (MST) of the
 graph. The graph is represented by an adjacency list, where each element adj[i] is a vector 
 containing vector of integers. Each vector represents an edge, with the first integer 
 denoting the endpoint of the edge and the second integer denoting the weight of the edge.
+
+# Implemented Minimum Spaning tree using Prims algorithm
 
 Example 1:
 
@@ -39,20 +47,25 @@ def spanningTree(V, adj):
     summ = 0
     priorityQ = [(0, 0)]
     
+    # create priority queue using min-heap
     heapify(priorityQ)
     
     while priorityQ:
-        wt, el = heappop(priorityQ) # min heap pop
-        if visited[el]:
+        # min heap pop - poped element will always have minimum weight
+        wt, el = heappop(priorityQ) 
+        if visited[el]: # if already visited then leave
             continue
-        visited[el] = True
+            
+        # if not visited, mark it visited and add the value to sum
+        visited[el] = True 
         summ += wt
-        
-        for adjel in adj[el]:
+
+        # check all the adjacent nodes of the poped node
+        for adjel in adj[el]: 
             adjn = adjel[0]
             adjw = adjel[1]
             if not visited[adjn]:
-                heappush(priorityQ,(adjw, adjn))
+                heappush(priorityQ,(adjw, adjn)) # push the node along with the weight in priority queue
     return summ
     
 if __name__ == "__main__":
