f78781db988444e1b90b3eafed5505f4a071ea2c
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-08-31 21:20:54 +0530

    Return the k possible array pairs with the smallest sums
    Return the {k} pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums with the given arrays

--- Heap/kSmallestPairs.py
+++ Heap/kSmallestPairs.py
@@ -28,6 +28,41 @@ Explanation: The first 2 pairs are returned from the sequence:
 import heapq
 
 def kSmallestPairs(nums1, nums2, k):
+    priorityQ = []
+    result = []
+    n = len(nums1)
+    m = len(nums2)
+
+    visited = [[False] * m for _ in range(n)]
+    heapq.heappush(priorityQ,(nums1[0] + nums2[0],(0, 0)))
+    visited[0][0] = True
+
+    while (k > 0 and priorityQ):
+        # Pop the smallest pair
+        current_sum, cords = heapq.heappop(priorityQ)
+        
+        i = cords[0]
+        j = cords[1]
+        # Append the corresponding pair to the result
+        result.append([nums1[i], nums2[j]])
+
+        # push (i, j+1) if possible
+        if(j+1 < m and not visited[i][j+1]):
+            heapq.heappush(priorityQ, (nums1[i]+nums2[j+1], (i, j+1)))
+            visited[i][j+1] = True
+
+        # push (i+1, j) if possible
+        if(i+1 < n and not visited[i+1][j]):
+            heapq.heappush(priorityQ, (nums1[i+1]+nums2[j], (i+1, j)))
+            visited[i+1][j] = True
+        
+        k -= 1
+            
+
+    return result
+
+
+def optimized_kSmallestPairs(nums1, nums2, k):
     # Initialize the min heap
     min_heap = []
     
@@ -36,12 +71,14 @@ def kSmallestPairs(nums1, nums2, k):
     for i in range(min(k, len(nums1))):  # Only need to consider at most k elements from nums1
         heapq.heappush(min_heap, (nums1[i] + nums2[0], i, 0))
     
+    # print(min_heap)
     result = []
     
     # Extract the smallest pairs from the heap
     while min_heap and len(result) < k:
         # Pop the smallest pair
         current_sum, i, j = heapq.heappop(min_heap)
+        # print(f"current_sum = {current_sum}, i = {i}, j = {j}")
         
         # Append the corresponding pair to the result
         result.append([nums1[i], nums2[j]])
@@ -60,3 +97,5 @@ if __name__ == "__main__":
     nums2 = [2, 4, 6]
     k = 3
     print(f"Return the {k} pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums:\n", kSmallestPairs(nums1, nums2, k))
+    
+    print(f"Return the {k} pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums:\n", optimized_kSmallestPairs(nums1, nums2, k))
