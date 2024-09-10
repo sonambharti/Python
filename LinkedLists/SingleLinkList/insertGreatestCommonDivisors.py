e259a7ab2ad6168ac909403dcb5fc2c4b51e0066
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-09-10 14:20:33 +0530

    Insert GCD node between every adjacent node in the linked list

--- LinkedLists/SingleLinkList/insertGreatestCommonDivisors.py
+++ LinkedLists/SingleLinkList/insertGreatestCommonDivisors.py
@@ -56,16 +56,24 @@ def create_linked_list(arr):
     
     
     
+# def gcd(a, b): # can cause segmentation fault due to recursion limit in python
+#     if a == 0:
+#         return b
+#     if b == 0:
+#         return a
+#     if a == b:
+#         return a
+#     if a > b:
+#         return gcd(a-b, b)
+#     return gcd(a, b-a)
+
+
 def gcd(a, b):
-    if a == 0:
-        return b
     if b == 0:
         return a
-    if a == b:
-        return a
-    if a > b:
-        return gcd(a-b, b)
-    return gcd(a, b-a)
+    return gcd(b, a%b)
+
+
     
     
 def insertGreatestCommonDivisors(head):
