9c37075aee6f48a1c956e79efc70ff87b890723a
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-11-20 23:14:32 +0530

    find minimum size of the set array to be removed.

--- Heap/minSetSize.py
+++ Heap/minSetSize.py
@@ -13,7 +13,10 @@ Input: arr = [3,3,3,3,5,5,5,2,2,7]
 Output: 2
 Explanation: Choosing {3,7} will make the new array [5,5,5,2,2] which has size 5 (i.e equal to half of the size of the old array).
 Possible sets of size 2 are {3,5},{3,2},{5,2}.
-Choosing set {2,7} is not possible as it will make the new array [3,3,3,3,5,5,5] which has a size greater than half of the size of the old array.
+Choosing set {2,7} is not possible as it will make the new array [3,3,3,3,5,5,5] which has a size greater than half of the size of
+the old array.
+
+
 Example 2:
 
 Input: arr = [7,7,7,7,7,7]
