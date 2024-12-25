d47b3d47622a6a5d87b7d5fa322b7c77f8c46e35
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-12-25 20:01:36 +0530

    Update TransposeMatrix.pyTranspose Matrix

--- Array/Matrix/TransposeMatrix.py
+++ Array/Matrix/TransposeMatrix.py
@@ -1,4 +1,4 @@
-
+# Transpose Matrix
 
 def transpose(A,B): 
     N = len(A[0])
@@ -7,7 +7,17 @@ def transpose(A,B):
         for j in range(M): 
             B[i][j] = A[j][i] 
     print(B)
-   
+
+def transposeInPlace(mat):
+    n = len(mat)
+    m = len(mat[0])
+
+    for i in range(n): 
+        for j in range(i): 
+            mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+    
+    return mat
+    
 if __name__ == "__main__":
     # take a 3x4 matrix    
     A = [[5, 8, 1, 2],
@@ -17,5 +27,6 @@ if __name__ == "__main__":
     B = [[0]*len(A) for _ in range(len(A[0]))]
     
     transpose(A,B)
+    print(transposeInPlace(A))
    
     
