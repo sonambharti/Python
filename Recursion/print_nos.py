98746dbf9c114906d4d5ed00407dc930692e7de1
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2026-08-08 00:53:47 +0530

    print 1 to n and n to 1

--- Recursion/print_nos.py
+++ Recursion/print_nos.py
@@ -1,11 +1,30 @@
 # print nos from 1 to n
-def print_nos(lst, indx, n):
+def print_nos1(lst, indx, n):
     if indx > n:
         return 
     lst.append(indx)
     print_nos(lst, indx+1, n)
     return lst
+
+def print_nos(n):
+    if n==1:
+        print(n, end =" ")
+        return 
+    
+    print_nos(n - 1)
+    print(n, end =" ")
+
+def print_nos3(n):
+    if n==1:
+        print(n, end =" ")
+        return 
+    
+    print(n, end =" ")
+    print_nos(n - 1)
     
 if __name__ == "__main__":
-    res = print_nos([], 1, 5) # print nos from 1 to 5
+    print_nos(5) # print nos from 1 to 5
+    res = print_nos1([], 1, 5) # print nos from 1 to 5
     print(res)
+    print_nos3(5) # print nos from 5 to 1
+    
