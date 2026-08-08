dbc5d5ac052c2212650f49ca0aee0cca95cf5dd3
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2026-08-08 20:07:03 +0530

    Add function to delete middle element from stack
    Implement a function to delete the middle element from a stack using recursion.

---
+++ Recursion/deleteMidElArr.py
@@ -0,0 +1,26 @@
+# Delete Middle Ellement from a stack
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+def solve(st, k):
+    if k==1:
+        st.pop()
+        return
+    
+    temp = st.pop()
+    solve(st, k-1)
+    st.append(temp)
+    return
+    
+def deleteMidElArr(st, n):
+    if len(st) == 0:
+        return
+    k = n//2
+    solve(st, k)
+    return
+    
+    
+if __name__ == "__main__":
+    st = [2,7,3,5]
+    deleteMidElArr(st, len(st))
+    print(st)
